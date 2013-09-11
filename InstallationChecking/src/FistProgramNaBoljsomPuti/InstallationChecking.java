4cd52eee200e9a550341eb61c42016fe175a5503
Grennata <natalja.griscenko@gmail.com>
2013-09-11 16:32:20 +0100

    InstallationChecking was added

---
+++ InstallationChecking/src/FistProgramNaBoljsomPuti/InstallationChecking.java
@@ -0,0 +1,14 @@
+package FistProgramNaBoljsomPuti;
+
+import java.io.*;
+
+public class InstallationChecking {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+	}
+
+}
